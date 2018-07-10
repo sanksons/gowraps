6576b9911d6f3d09c50f8d53cf4dfba0567f08de
Sankalp Bhatt <glitter.sankalp@gmail.com>
2018-07-10 13:59:51 +0530

    Update Imge utils

--- timer/timer.go
+++ timer/timer.go
@@ -5,3 +5,15 @@ import "time"
 func GetHttpTime(t time.Time) string {
 	return t.Format(time.RFC1123)
 }
+
+func GetCurrentTime(zone string) (time.Time, error) {
+	if zone == "" {
+		zone = "UTC"
+	}
+	cTime := time.Now()
+	location, locErr := time.LoadLocation(zone)
+	if locErr != nil {
+		return cTime, locErr
+	}
+	return cTime.In(location), nil
+}
