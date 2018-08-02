cc11af82cb49ff73ecc14f9b0ffa4202c7b00ab0
Sankalp Bhatt <glitter.sankalp@gmail.com>
2018-08-02 15:48:52 +0530

    increase support fro ToInt

--- convert/convert.go
+++ convert/convert.go
@@ -8,13 +8,18 @@ import (
 func ToInt(i interface{}) (int, error) {
 	switch v := i.(type) {
 
+	case int32:
+		return int(v), nil
+	case int64:
+		return int(v), nil
 	case int:
 		return int(v), nil
 	case float64:
 		return int(v), nil
+	case float32:
+		return int(v), nil
 	case string:
 		return strconv.Atoi(v)
-
 	default:
 		return 0, fmt.Errorf("I don't know about type %T!", v)
 	}
